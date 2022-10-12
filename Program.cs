f52ca5e4c9a41f91386b2f2ed95085d2ffd91fe1
Alex Puzanov <6702715@gmail.com>
2022-10-12 12:44:56 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("CSVReader v.0.0.1");
+Console.ResetColor();
+
+string path = @"/home/user/Projects/CSVReader/employees.csv";
+
+if (File.Exists(path))
+{
+    using (StreamReader reader = new StreamReader(path))
+    {
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            System.Console.WriteLine(line);
+        }
+    }
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    System.Console.WriteLine("The file " + path + " does not exist!");
+    Console.ResetColor();
+}
